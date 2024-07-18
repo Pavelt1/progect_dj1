8d9197fffd0a97caaa13a1ced0d3857f7a25fb8b
Pavel11 <paveltarasov11@mail.ru>
2024-07-18 20:41:57 +0300

    new commit

--- StartDjango/settings.py
+++ StartDjango/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # здесь мы подключаем наши приложения
-    'app'
+    'app',
+    'calculator'
 ]
 
 MIDDLEWARE = [
--- StartDjango/urls.py
+++ StartDjango/urls.py
@@ -17,13 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from app.views import home_view,time_view,workdir_view
+from calculator.views import recipe
 
 
 urlpatterns = [
     path('', home_view, name='home'),
-    # Раскомментируйте код, чтобы данные урлы 
-    # обрабатывались Django
     path('current_time/', time_view, name='time'),
     path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
+    path('recipe/<name>/', recipe)
 ]
